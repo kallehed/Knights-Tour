a90d4dad30612d272742cb36d25344fbdc33cb93
kallehed <kallehed@gmail.com>
2022-11-26 12:41:45 +0100

    Tweaked details, got better comments. Cleaned things up

--- knightstour/knightstour.cpp
+++ knightstour/knightstour.cpp
@@ -2,20 +2,17 @@
 #include <array>
 #include <chrono>
 
-// to width 1000 tested
-// height 80 or more seems to be the same
+/* notes :
+        Width to 3000 tested.
+        Height 80 or more seems to be the same */
+
 
 // CUSTOM COMPILE-TIME VARIABLES TO SET
+///////////////////////////////////////////////////////
+constexpr inline int MAX_WIDTH = 200;
 constexpr inline int MAX_HEIGHT = 80;
-constexpr inline int MAX_WIDTH = 2000;
-constexpr inline bool POSSIBLE_TO_DRAW_BLOCKS = true;
-constexpr inline bool CAN_DRAW_HALT = true;
+///////////////////////////////////////////////////////
 
-//  14987ms
-
-// better ordering of moves: 
-
-// faster  14455
 
 // program:
 
@@ -38,21 +35,20 @@ void draw_blocks(const int width, const int height)
     }
 }
 
-// in position in matrix with width and height
-bool halts(int i, int j, const int width, const int height, const bool draw)
+// simulates knight in position, with certain width and height.
+// 'check_if_went_above' : false => it should finish when halted, true => it should finish when going above start.
+// 'draw' makes it draw each movement of the knight
+template <bool check_if_went_above = false>
+bool halts(const int start_i, const int start_j, const int width, const int height, const bool draw)
 {
-    bool return_val = true;
-    
+    bool return_val = !check_if_went_above;
+    int i = start_i, j = start_j;
     while (true) {
-        //std::cout << "pos: " << i << ' ' << j << " val: " << j+1 + i*3 << '\n';
         matrix[i][j] = true;
 
-        // print map
-        if constexpr (POSSIBLE_TO_DRAW_BLOCKS) {
-            if (draw) {
-                draw_blocks(width, height);
-                std::cin.get();
-            }
+        if (draw) {
+            draw_blocks(width, height);
+            std::cin.get();
         }
 
         { // loop unrolling version
@@ -121,9 +117,17 @@ bool halts(int i, int j, const int width, const int height, const bool draw)
         
         } GOTO_AFTER_MOVE:
 
-        if (i > top) {
-            return_val = false;
-            break;
+        if constexpr (check_if_went_above) {
+            if (i > start_i) {
+                return_val = true;
+                break;
+            }
+        }
+        else {
+            if (i > top) {
+                return_val = false;
+                break;
+            }
         }
     }
     for (int i = 0; i < height; ++i) { // clear just the cells that could have been filled
@@ -135,108 +139,15 @@ bool halts(int i, int j, const int width, const int height, const bool draw)
     return return_val;
 }
 
-// says whether position with matrix of certain width and height goes above it's own position or not
-int went_above(int i, int j, const int width, const int height)
-{
-    bool went_above = false;
-    int start_i = i;
-
-    while (true) {
-        //std::cout << "pos: " << i << ' ' << j << " val: " << j+1 + i*3 << '\n';
-        matrix[i][j] = true;
-
-        { // loop unrolling version
-            if (i >= 2) {
-                if (j >= 1) {
-                    if (!matrix[i - 2][j - 1]) {
-                        i = i - 2;
-                        j = j - 1;
-                        goto GOTO_AFTER_MOVE;
-                    }
-                }
-                if (j + 1 < width) {
-                    if (!matrix[i - 2][j + 1]) {
-                        i = i - 2;
-                        j = j + 1;
-                        goto GOTO_AFTER_MOVE;
-                    }
-                }
-            }
-            if (i >= 1) {
-                if (j >= 2) {
-                    if (!matrix[i - 1][j - 2]) {
-                        i = i - 1;
-                        j = j - 2;
-                        goto GOTO_AFTER_MOVE;
-                    }
-                }
-                if (j + 2 < width) {
-                    if (!matrix[i - 1][j + 2]) {
-                        i = i - 1;
-                        j = j + 2;
-                        goto GOTO_AFTER_MOVE;
-                    }
-                }
-            }
-            if (j >= 2) {
-                if (!matrix[i + 1][j - 2]) {
-                    i = i + 1;
-                    j = j - 2;
-                    goto GOTO_AFTER_MOVE;
-                }
-            }
-            if (j + 2 < width) {
-                if (!matrix[i + 1][j + 2]) {
-                    i = i + 1;
-                    j = j + 2;
-                    goto GOTO_AFTER_MOVE;
-                }
-            }
-            if (j >= 1) {
-                if (!matrix[i + 2][j - 1]) {
-                    i = i + 2;
-                    j = j - 1;
-                    goto GOTO_AFTER_MOVE;
-                }
-            }
-            if (j + 1 < width) {
-                if (!matrix[i + 2][j + 1]) {
-                    i = i + 2;
-                    j = j + 1;
-                    goto GOTO_AFTER_MOVE;
-                }
-            }
-
-            break;
-
-        } GOTO_AFTER_MOVE:
-
-        if (i > start_i) {
-            went_above = true;
-            break;
-        }
-    }
-    for (int i = 0; i < height; ++i) { // clear just the cells that could have been filled
-        for (int j = 0; j < width; ++j) {
-            matrix[i][j] = false;
-        }
-    }
-
-    return went_above;
-}
-
-// in  width and height, how many halt?
+// in a certain width and height, how many knights halt?
 int how_many_not_halt(const int width, const int height, const bool draw, const bool say_pos_of_not_halt) 
 {
     int not_halts = 0;
     for (int i = top; i >= 0; --i) {
         for (int j = 0; j < width; ++j) {
             if (halts(i, j, width, height, false)) {
-                if constexpr (CAN_DRAW_HALT) {
-                    if (draw) std::cout << "H ";
-                }
+                if (draw) std::cout << "H ";
             }
-
             else {
                 if (draw) std::cout << "| ";
                 if (say_pos_of_not_halt) {
@@ -250,12 +161,13 @@ int how_many_not_halt(const int width, const int height, const bool draw, const 
     return not_halts;
 }
 
+// for a certain width and height, gives how many went above their start position
 int how_many_went_above(const int width, const int height, const bool draw)
 {
     int nr_went_above = 0;
     for (int i = top; i >= 0; --i) {
         for (int j = 0; j < width; ++j) {
-            if (went_above(i, j, width, height)) {
+            if (halts<true>(i, j, width, height, false)) {
                 ++nr_went_above;
                 if (draw) std::cout << "|";
             }
@@ -265,24 +177,24 @@ int how_many_went_above(const int width, const int height, const bool draw)
         }
         if (draw) std::cout << '\n';
     }
-    ///*if (draw)*/ std::cout << "Width: " << width << ",Total went above : " << nr_went_above << '\n';
     return nr_went_above;
 }
 
+// prints all forever-escaping knights for each width
 void test_widths_for_halts()
 {
     for (int width = 1; width <= MAX_WIDTH; ++width) {
         std::cout << "Width: " << width << '\n';
         int num = how_many_not_halt(width, MAX_HEIGHT, false, true);
-        //std::cout <<"Total not halted: " << num << '\n';
     }
 }
 
+// computes and prints how many knights went above in all widths
 void test_widths_for_went_above()
 {
     for (int width = 1; width <= MAX_WIDTH; ++width) {
         int num = how_many_went_above(width, MAX_HEIGHT, false);
-        std::cout << num << ',';
+        std::cout << "width:" << width << " went above themselves: " << num << '\n';
     }
 }
 
@@ -295,9 +207,10 @@ int main()
     using namespace std::chrono;
     auto start = high_resolution_clock::now();
 
+    // modifiable code
     {
-        //how_many_went_above(MAX_WIDTH, MAX_HEIGHT, true);
-        test_widths_for_went_above();
+        how_many_went_above(MAX_WIDTH, MAX_HEIGHT, true);
+        //test_widths_for_went_above();
         //test_widths_for_halts();
     }
 
